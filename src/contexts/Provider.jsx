cb54221987145169401ffc4bd5fdb097eb0bae42
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-20 14:40:55 +0330

    Initial setup of EditNote page

--- src/contexts/Provider.jsx
+++ src/contexts/Provider.jsx
@@ -1,10 +1,13 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
-import { ADD_NOTE, DELETE_NOTE, UPDATE_NOTE } from "./actionTypes";
+import { ADD_NOTE, DELETE_NOTE, EDIT_NOTE, UPDATE_NOTE } from "./actionTypes";
 
 const notesReducer = (notes, { type, payload }) => {
   switch (type) {
     case ADD_NOTE:
       return [...notes, payload];
+
+    case EDIT_NOTE:
+      return notes.map((note) => (note.id === payload.id ? payload : note));
 
     case DELETE_NOTE:
       return notes.filter((note) => note.id !== payload);
@@ -23,7 +26,7 @@ const initialState = JSON.parse(localStorage.getItem("notes")) || [];
 
 export default function Provider({ children }) {
   const [notes, dispatch] = useReducer(notesReducer, initialState);
-  console.log(notes)
+  console.log(notes);
 
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
