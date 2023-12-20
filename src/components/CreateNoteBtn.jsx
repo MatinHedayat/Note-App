93877c0505845e161b00044e498b855342d11252
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-20 20:45:19 +0330

    Update path link of CreateNoteBtn component

--- src/components/CreateNoteBtn.jsx
+++ src/components/CreateNoteBtn.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function CreateNoteBtn() {
   return (
-    <Link to='add-note'>
+    <Link to='/add-note'>
         <button className='bg-slate-700 w-16 h-16 fixed bottom-40 right-6 flex items-center justify-center outline-2 outline-slate-700 rounded-full plus-shadow min-[875px]:inset-x-1/2 min-[875px]:-translate-x-1/2'>
           <FaPlus className='text-aliceblue' />
 
