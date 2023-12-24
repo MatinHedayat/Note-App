b301a59b9a86400d374665665b61ff53cc2fc6da
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-24 17:01:16 +0330

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import {
   AddNote,
   Bookmarks,
   EditNote,
-  Hint,
-  History,
-  Login,
   NoteList,
   User,
-} from "./components/pages/exporter";
+} from "./pages/exporter";
+import { AnimatePresence } from "framer-motion";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
-      <Routes>
-        <Route path='/' element={<NoteList />} />
-        <Route path='add-note' element={<AddNote />} />
-        <Route path='edit-note' element={<EditNote />} />
-        <Route path='user' element={<User />} />
-        <Route path='login' element={<Login />} />
-        <Route path='hint' element={<Hint />} />
-        <Route path='bookmarks' element={<Bookmarks />} />
-      </Routes>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<NoteList />} />
+          <Route path='/add-note' element={<AddNote />} />
+          <Route path='/edit-note' element={<EditNote />} />
+          <Route path='/bookmarks' element={<Bookmarks />} />
+          <Route path='/user' element={<User />} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
