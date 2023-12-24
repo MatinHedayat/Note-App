28af3d769c6afabb35693525f294baffa8d12b87
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-24 17:00:42 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
       sans: ["Poppins"],
@@ -9,20 +10,23 @@ export default {
       colors: {
         aliceblue: "aliceblue",
       },
+      screens: {
+        ss: "400px",
+      },
       keyframes: {
         rotateOpacity: {
-          "0%": { opacity: '0', transform: 'rotate(45deg)' },
-          "100%": { opacity: '1', transform: 'rotate(0)' },
+          "0%": { opacity: "0", transform: "rotate(45deg)" },
+          "100%": { opacity: "1", transform: "rotate(0)" },
         },
         opacity: {
-          "0%": { opacity: '0' },
-          "100%": { opacity: '1'  },
-        }
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
       animation: {
-        rotateOpacity: 'rotateOpacity 0.3s ease-in-out 1',
-        opacity: 'opacity 0.2s ease-in-out 1',
-      }
+        rotateOpacity: "rotateOpacity 0.3s ease-in-out 1",
+        opacity: "opacity 0.2s ease-in-out 1",
+      },
     },
   },
   plugins: [],
